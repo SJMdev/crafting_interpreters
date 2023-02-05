ce29067d7d5056de3c99a13ac650f1a8eb8900ce
sjmdev <sjorsmallon@gmail.com>
2023-02-05 11:16:38 +0100

    [feat] binary operations!

--- c/main.c
+++ c/main.c
@@ -9,9 +9,24 @@ int main(int argc, const char* argv[]) {
     Chunk chunk;
     initChunk(&chunk);
 
+    // -( (1.2 + 3.4) / 5.6))
+    // this adds a constant to the constants array,
+    // writes the CONSTANT op in the bytecode array, as well as the index it has in the constants array.
     int constant = addConstant(&chunk, 1.2);
     writeChunk(&chunk, OP_CONSTANT, 123);
     writeChunk(&chunk, constant,  123);
+
+    constant = addConstant(&chunk, 3.4);    
+    writeChunk(&chunk, OP_CONSTANT, 123);
+    writeChunk(&chunk, constant,  123);
+
+    writeChunk(&chunk, OP_ADD, 123);
+
+    constant = addConstant(&chunk, 5.6);
+    writeChunk(&chunk, OP_CONSTANT, 123);
+    writeChunk(&chunk, constant, 123);
+
+    writeChunk(&chunk, OP_DIVIDE, 123);
     writeChunk(&chunk, OP_NEGATE, 123);
     writeChunk(&chunk, OP_RETURN, 123);
     disassembleChunk(&chunk, "test chunk");
